64fd08d76934a8c48b5e9bdf516e6f1a3004a95c
Duncan Maina <duncandevs@gmail.com>
2017-03-15 11:13:59 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
+require 'pry'
 def count_elements(array)
   # code goes here
+  result = {}
+  array.each do |element|
+    puts element
+    result[element] = array.count(element)
+  end
+  result
 end
- 
